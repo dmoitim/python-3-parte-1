5ef37aa33b17b40d59622987b3494748b154d960
Devair Luiz Moitim <dmoitim@gmail.com>
2020-08-22 15:23:53 -0300

    Quarta versão, trata limite do número informado

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,14 +3,16 @@ print("* Bem vindo ao jogo de Adivinhação! *")
 print("*************************************")
 
 total_de_tentativas = 3
-rodada = 1
 numero_secreto = 42
 
-
-while (rodada <= total_de_tentativas):
+for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute = int(input("Digite o seu número: "))
+    chute = int(input("Digite o seu número ente 1 e 100: "))
     print("Você digitou", chute)
+
+    if (chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100.")
+        continue
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
@@ -18,6 +20,7 @@ while (rodada <= total_de_tentativas):
 
     if (acertou):
         print("Você acertou!")
+        break
     else:
         if (maior):
             print("Você errou. Seu chute foi maior que o número secreto.")
