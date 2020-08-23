229a36ed66e9644f084845058bb20d1e2a98023c
Devair Luiz Moitim <dmoitim@gmail.com>
2020-08-23 10:04:37 -0300

    Sexta versão, níveis de dificuldade

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,8 +4,20 @@ print("*************************************")
 print("* Bem vindo ao jogo de Adivinhação! *")
 print("*************************************")
 
-total_de_tentativas = 3
+total_de_tentativas = 0
 numero_secreto = random.randrange(1, 101)
+
+print("Qual nível de dificuldade?")
+print("(1) Fácil   (2) Médio   (3) Difícil")
+
+nivel = int(input("Defina o nível desejado: "))
+
+if (nivel == 1):
+    total_de_tentativas = 20
+elif (nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
